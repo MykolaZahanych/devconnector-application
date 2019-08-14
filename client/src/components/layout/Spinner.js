75fd0d7cfbcee3095f1d481375e615c693507f84
Mykola Zahanych <35174437+MykolaZahanych@users.noreply.github.com>
2019-08-14 13:49:37 +0200

    fix type error

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,11 +1,11 @@
-import React, { Fragmnet } from 'react'
+import React, { Fragment } from 'react'
 import spinner from './spinner.png'
 
 export default () => (
-  <Fragmnet>
+  <Fragment>
     <img
       src={spinner}
       style={{ width: '200px', margin: 'auto', display: 'block' }}
       alt='Loading' />
-  </Fragmnet>
+  </Fragment>
 )
