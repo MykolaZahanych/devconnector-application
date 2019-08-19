6592a8fa2026c80f6cc336ea1a71c3e376247b30
Mykola Zahanych <35174437+MykolaZahanych@users.noreply.github.com>
2019-08-19 13:38:13 +0200

    Prepared for deployment

--- server.js
+++ server.js
@@ -10,14 +10,21 @@ connectDB();
 // init Middleware
 app.use(express.json({extended: false}))
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Seerve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 // port number
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
